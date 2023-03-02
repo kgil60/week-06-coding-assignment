823c63795604bf7961e689989dc9d121a7b39e05
kgil60 <gilvaryke@gmail.com>
2023-03-01 20:44:48 -0500

    make game playable with 52 cards

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ class Player {
 class Deck {
     constructor() {
         this.cards = [
-            // {cardName: 'Ace of Hearts', value: 1},
+            {cardName: 'Ace of Hearts', value: 1},
             {cardName: 'Two of Hearts', value: 2},
             {cardName: 'Three of Hearts', value: 3},
             {cardName: 'Four of Hearts', value: 4},
@@ -51,6 +51,45 @@ class Deck {
             {cardName: 'Jack of Hearts', value: 11},
             {cardName: 'Queen of Hearts', value: 12},
             {cardName: 'King of Hearts', value: 13},
+            {cardName: 'Ace of Clubs', value: 1},
+            {cardName: 'Two of Clubs', value: 2},
+            {cardName: 'Three of Clubs', value: 3},
+            {cardName: 'Four of Clubs', value: 4},
+            {cardName: 'Five of Clubs', value: 5},
+            {cardName: 'Six of Clubs', value: 6},
+            {cardName: 'Seven of Clubs', value: 7},
+            {cardName: 'Eight of Clubs', value: 8},
+            {cardName: 'Nine of Clubs', value: 9},
+            {cardName: 'Ten of Clubs', value: 10},
+            {cardName: 'Jack of Clubs', value: 11},
+            {cardName: 'Queen of Clubs', value: 12},
+            {cardName: 'King of Clubs', value: 13},
+            {cardName: 'Ace of Diamonds', value: 1},
+            {cardName: 'Two of Diamonds', value: 2},
+            {cardName: 'Three of Diamonds', value: 3},
+            {cardName: 'Four of Diamonds', value: 4},
+            {cardName: 'Five of Diamonds', value: 5},
+            {cardName: 'Six of Diamonds', value: 6},
+            {cardName: 'Seven of Diamonds', value: 7},
+            {cardName: 'Eight of Diamonds', value: 8},
+            {cardName: 'Nine of Diamonds', value: 9},
+            {cardName: 'Ten of Diamonds', value: 10},
+            {cardName: 'Jack of Diamonds', value: 11},
+            {cardName: 'Queen of Diamonds', value: 12},
+            {cardName: 'King of Diamonds', value: 13},
+            {cardName: 'Ace of Spades', value: 1},
+            {cardName: 'Two of Spades', value: 2},
+            {cardName: 'Three of Spades', value: 3},
+            {cardName: 'Four of Spades', value: 4},
+            {cardName: 'Five of Spades', value: 5}, 
+            {cardName: 'Six of Spades', value: 6},
+            {cardName: 'Seven of Spades', value: 7},
+            {cardName: 'Eight of Spades', value: 8},
+            {cardName: 'Nine of Spades', value: 9},
+            {cardName: 'Ten of Spades', value: 10},
+            {cardName: 'Jack of Spades', value: 11},
+            {cardName: 'Queen of Spades', value: 12},
+            {cardName: 'King of Spades', value: 13}
         ];
     }
 }
@@ -105,6 +144,9 @@ class Main {
             // console.log(this.player1.score, this.player2.score);
         }
 
+        // console.log(this.player1.deck);
+        // console.log(this.player2.deck);
+
         alert(`The game is over. Let\'s compare scores...\n\nPlayer 1: ${this.player1.score} points.\nPlayer 2: ${this.player2.score} points.`)
 
         let winner = this.compareScores(this.player1, this.player2);
@@ -120,8 +162,8 @@ class Main {
     deal() {
         let dealtCards = []
 
-        while (this.player1.deck.length < 6) {
-            let chosenCard = Math.floor(Math.random() * 12 + 0)
+        while (this.player1.deck.length < 26) {
+            let chosenCard = Math.floor(Math.random() * 52)
             
             if (this.deck.cards[chosenCard] && dealtCards.includes(this.deck.cards[chosenCard]) === false) {
                 this.player1.deck.push(this.deck.cards[chosenCard]);
@@ -131,8 +173,8 @@ class Main {
 
         // console.log(this.player1.deck);
 
-        while (this.player2.deck.length < 6) {
-            let chosenCard = Math.floor(Math.random() * 12)
+        while (this.player2.deck.length < 26) {
+            let chosenCard = Math.floor(Math.random() * 52)
 
             if (this.deck.cards[chosenCard] && dealtCards.includes(this.deck.cards[chosenCard]) === false) {
                 this.player2.deck.push(this.deck.cards[chosenCard]);
